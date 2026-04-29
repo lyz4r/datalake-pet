532f98a0a7504c921566fd6800b8ceab8d026db0
idagapov <aid19.02.06@mail.ru>
2026-04-29 16:18:11 +0300

    added bybit producer

---
+++ producers/bybit_ws_producer.py
@@ -0,0 +1,39 @@
+import asyncio
+import orjson
+import logging
+import websockets
+
+
+from utils.kafka_client import KafkaClient
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(levelname)s] %(message)s")
+log = logging.getLogger(__name__)
+
+WS_URL = "wss://stream.bybit.com/v5/public/spot"
+SUBSCRIBE_MSG = {
+    "op": "subscribe",
+    "args": ["tickers.BTCUSDT"]
+}
+KAFKA_TOPIC = "crypto.tickers"
+
+
+async def connect_to_bybit(uri: str, subscribe_msg: dict):
+    log.info("Connecting to %s", uri)
+    with KafkaClient() as producer:
+        try:
+            async with websockets.connect(uri) as ws:
+                log.info("Connected")
+                await ws.send(orjson.dumps(subscribe_msg))
+                log.debug("Sent subscription: %s", subscribe_msg)
+                async for raw in ws:
+                    msg = orjson.loads(raw)
+                    producer.send(KAFKA_TOPIC, value=msg)
+        except websockets.exceptions.InvalidStatus as e:
+            log.error(f"Крымчанам соболезную: {e}")
+        except Exception as e:
+            log.error(f"Could not connect to bybit WebSocket: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(connect_to_bybit(WS_URL, SUBSCRIBE_MSG))
---
+++ producers/utils/kafka_client.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+
+import json
+import os
+
+
+class KafkaClient:
+    def __init__(self):
+        self.producer = KafkaProducer(
+            bootstrap_servers=os.environ["KAFKA_BOOTSTRAP_SERVERS"],
+            value_serializer=lambda v: json.dumps(v, default=str).encode(),
+            key_serializer=lambda k: k.encode() if k else None,
+            acks="all",
+            retries=5,
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.producer.flush(timeout=10)
+        self.producer.close()
